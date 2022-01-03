fb516fed64e22707dc4ab40a7e77983918652241
florimondmanca <florimond.manca@fairness.coop>
2022-01-03 14:15:42 +0100

    Finalize
    * Exclude from schema
    
    * Fix index endpoint test
    
    * Add docs_url constant/setting

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,7 @@ import pytest
 
 
 @pytest.mark.asyncio
-async def test_app(client: httpx.AsyncClient) -> None:
+async def test_index(client: httpx.AsyncClient) -> None:
     response = await client.get("/")
-    assert response.json() == {"message": "Hello, world!"}
+    assert response.status_code == 307
+    assert response.headers["Location"] == "/docs"
