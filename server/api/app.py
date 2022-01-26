16b0d39bf9bd8d6e4d1da82e2bb8fd79fb17a2e0
Mathieu Agopian <mathieu@agopian.info>
2022-01-26 15:31:54 +0100

    Add CORS for local dev

--- server/api/app.py
+++ server/api/app.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from server.config import Settings
 from server.config.di import resolve
 from server.infrastructure.database import Database
 
 from .routes import router
+
+
+origins = [
+    "http://localhost:3000",
+]
 
 
 def create_app() -> FastAPI:
@@ -14,6 +20,13 @@ def create_app() -> FastAPI:
     app = FastAPI(
         on_startup=[db.create_all],
         docs_url=settings.docs_url,
+    )
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     app.include_router(router)
