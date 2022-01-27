2f9297fb6542e6052b478932558006d8fbed7668
Mathieu Agopian <mathieu@agopian.info>
2022-01-27 09:24:06 +0100

    Add CORS for local dev (#36)
    * Add CORS for local dev
    
    * Fix whitespace
    
    * Add test
    
    * Add a check in the app tests for Access-Control-Allow-Credentials
    
    Co-authored-by: florimondmanca <florimond.manca@fairness.coop>

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,3 +7,30 @@ async def test_index(client: httpx.AsyncClient) -> None:
     response = await client.get("/")
     assert response.status_code == 307
     assert response.headers["Location"] == "/docs"
+
+
+@pytest.mark.parametrize("origin", ["http://localhost:3000"])
+@pytest.mark.asyncio
+async def test_cors(client: httpx.AsyncClient, origin: str) -> None:
+    headers = {
+        "Origin": origin,
+        "Access-Control-Request-Method": "POST",
+        "Access-Control-Request-Headers": "Content-Type",
+    }
+    response = await client.options("/", headers=headers)
+    assert response.status_code == 200
+    assert response.headers["Access-Control-Allow-Origin"] == origin
+    assert "POST" in response.headers["Access-Control-Allow-Methods"]
+    assert response.headers["Access-Control-Allow-Credentials"]
+    assert response.headers["Access-Control-Allow-Headers"] == "Content-Type"
+
+
+@pytest.mark.asyncio
+async def test_cors_disallowed_origin(client: httpx.AsyncClient) -> None:
+    headers = {
+        "Origin": "http://disallowed.com",
+        "Access-Control-Request-Method": "POST",
+        "Access-Control-Request-Headers": "Content-Type",
+    }
+    response = await client.options("/", headers=headers)
+    assert response.status_code == 400
