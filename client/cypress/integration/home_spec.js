c6d3ceab34e8326016417ad498bc47da22b049e9
Mathieu Agopian <mathieu@agopian.info>
2022-02-02 16:09:24 +0100

    Fix form to match server's expectations

--- client/cypress/integration/home_spec.js
+++ client/cypress/integration/home_spec.js
@@ -2,17 +2,21 @@ const server = "http://127.0.0.1:3579";
 
 describe("Basic form submission", () => {
   it("Visits the home page", () => {
+    const title = "Un nom de jeu de données";
+    const description = "Une longue\ndescription de jeu\nde données";
     cy.visit("/");
 
-    cy.get("input[name=name]")
-      .type("Some Random Name")
-      .should("have.value", "Some Random Name");
+    cy.get("#title").type(title).should("have.value", title);
+    cy.get("#description").type(description).should("have.value", description);
 
     cy.intercept("POST", `${server}/datasets/`).as("new-dataset");
     cy.get("button").click().contains("loading...");
     cy.wait("@new-dataset").should(({ request, response }) => {
       expect(request.method).to.equal("POST");
-      expect(response.statusCode).to.equal(200);
+      expect(response.statusCode).to.equal(201);
+      expect(response.body.title).to.equal(title);
+      expect(response.body.description).to.equal(description);
+      expect(response.body).to.have.property("id");
     });
   });
 });
