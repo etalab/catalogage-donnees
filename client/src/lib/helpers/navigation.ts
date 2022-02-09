c2509482d8114559d4c228ebc23a4a2aa05ca6f5
florimondmanca <florimond.manca@fairness.coop>
2022-02-09 16:02:46 +0100

    Fix and refactor header nav

---
+++ client/src/lib/helpers/navigation.ts
@@ -0,0 +1,16 @@
+import type { NavItem } from "src/definitions/navigation";
+
+export const navigationItems: NavItem[] = [
+  {
+    label: "Rechercher",
+    href: "/",
+  },
+  {
+    label: "Contribuer",
+    href: "/contribuer",
+  },
+  {
+    label: "Mon espace",
+    href: "/mon-espace",
+  },
+];
