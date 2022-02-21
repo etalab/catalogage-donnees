77820a2a2ed57743702f31cfab4f5dd14d5ad219
florimondmanca <florimond.manca@fairness.coop>
2022-02-21 16:05:13 +0100

    Add edit link and empty page

--- client/src/routes/fiches/[id]/index.svelte
+++ client/src/routes/fiches/[id]/index.svelte
@@ -1,1 +1,13 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  $: editUrl = `/fiches/${$page.params.id}/edit`;
+</script>
+
 <h2>Fiche de données</h2>
+
+<div>
+  <a href={editUrl} class="fr-link" title="Modifier cette fiche de données">
+    Modifier
+  </a>
+</div>
