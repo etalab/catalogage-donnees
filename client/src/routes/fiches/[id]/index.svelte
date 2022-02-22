d8c51770fac47f9935880fbbe5b6dbf08727903c
florimondmanca <florimond.manca@fairness.coop>
2022-02-22 16:44:43 +0100

    Drop breadcrumbs
    Closes #84

--- client/src/routes/fiches/[id]/index.svelte
+++ client/src/routes/fiches/[id]/index.svelte
@@ -6,7 +6,7 @@
   const editUrl = `/fiches/${id}/edit`;
 </script>
 
-<h2>Fiche de données</h2>
+<h2 class="fr-mt-9w">Fiche de données</h2>
 
 <div>
   <a href={editUrl} class="fr-link" title="Modifier cette fiche de données">
