2b70dfd8464d28cf8822da70b835846a66b77445
florimondmanca <florimond.manca@fairness.coop>
2022-02-23 12:20:39 +0100

    Begin dataset detail page

--- client/src/routes/fiches/[id]/index.svelte
+++ client/src/routes/fiches/[id]/index.svelte
@@ -1,18 +1,179 @@
+<script context="module" lang="ts">
+  import type { Load } from "@sveltejs/kit";
+  import { getDatasetByID } from "$lib/repositories/datasets";
+
+  export const load: Load = async ({ fetch, params }) => {
+    const dataset = await getDatasetByID({ fetch, id: params.id });
+    return {
+      props: {
+        dataset,
+      },
+    };
+  };
+</script>
+
 <script lang="ts">
-  import { page } from "$app/stores";
   import paths from "$lib/paths";
+  import type { Dataset } from "src/definitions/datasets";
 
-  const id = $page.params.id;
+  export let dataset: Dataset;
 
-  const editUrl = paths.datasetEdit({ id });
+  const editUrl = paths.datasetEdit({ id : dataset.id});
 </script>
 
 <section class="fr-container fr-mt-9w">
-  <h2>Fiche de données</h2>
+  <div class="fr-grid-row fr-grid-row--middle fr-mb-3w">
+    <div class="fr-col-2">
+      <p class="fr-logo" title="république française">
+        {@html "Ministère<br />de la culture"}
+      </p>
+    </div>
+    <div class="fr-col">
+      <p class="fr-mb-0 fr-text-mention--grey">Ministère de la culture</p>
+      <h1>
+        {dataset.title}
+      </h1>
+    </div>
+  </div>
 
-  <div>
-    <a href={editUrl} class="fr-link" title="Modifier cette fiche de données">
-      Modifier
-    </a>
+  <ul
+    class="fr-grid-row fr-grid-row--right fr-btns-group fr-btns-group--inline fr-btns-group--icon-right"
+  >
+    <li>
+      <a
+        href={editUrl}
+        class="fr-btn fr-btn--secondary fr-fi-edit-fill"
+        title="Proposer une modification pour cette fiche de données"
+      >
+        Proposer une modification
+      </a>
+    </li>
+    <li>
+      <button
+        class="fr-btn fr-btn--secondary fr-fi-mail-fill"
+        title="Contacter le producter du jeu de données"
+      >
+        Contacter le producteur
+      </button>
+    </li>
+    <li>
+      <button
+        class="fr-btn fr-btn--secondary fr-fi-eye-fill"
+        title="Recevoir des alertes lors d'une modification à cette fiche de données"
+      >
+        Suivre
+      </button>
+    </li>
+  </ul>
+</section>
+
+<section class="fr-container">
+  <div class="fr-grid-row fr-grid-row--gutters">
+    <div class="fr-col-4">
+      <p>Producteur : DRAC Bretagne</p>
+      <p>Couverture géographique : France métropolitaine</p>
+    </div>
+    <div class="fr-col fr-tabs">
+      <ul
+        class="fr-tabs__list"
+        role="tablist"
+        aria-label="Onglets de navigation dans la fiche de données"
+      >
+        <li role="presentation">
+          <button
+            id="tabpanel-404"
+            class="fr-tabs__tab"
+            tabindex="0"
+            role="tab"
+            aria-selected="true"
+            aria-controls="tabpanel-404-panel"
+          >
+            Résumé
+          </button>
+        </li>
+        <li role="presentation">
+          <button
+            id="tabpanel-405"
+            class="fr-tabs__tab"
+            tabindex="-1"
+            role="tab"
+            aria-selected="false"
+            aria-controls="tabpanel-405-panel"
+          >
+            Sources
+          </button>
+        </li>
+        <li role="presentation">
+          <button
+            id="tabpanel-406"
+            class="fr-tabs__tab"
+            tabindex="-1"
+            role="tab"
+            aria-selected="false"
+            aria-controls="tabpanel-406-panel"
+          >
+            Contenu
+          </button>
+        </li>
+        <li role="presentation">
+          <button
+            id="tabpanel-407"
+            class="fr-tabs__tab"
+            tabindex="-1"
+            role="tab"
+            aria-selected="false"
+            aria-controls="tabpanel-407-panel"
+          >
+            Discussion (9)
+          </button>
+        </li>
+      </ul>
+
+      <div
+        id="tabpanel-404-panel"
+        class="fr-tabs__panel fr-tabs__panel--selected"
+        role="tabpanel"
+        aria-labelledby="tabpanel-404"
+        tabindex="0"
+      >
+        <p>
+          <strong> Propos liminaires </strong>
+        </p>
+        <p>
+          Conformément au décret n° AAAA-BBBB du DD mm YYYY relatif à la
+          publication de...
+        </p>
+      </div>
+
+      <div
+        id="tabpanel-405-panel"
+        class="fr-tabs__panel"
+        role="tabpanel"
+        aria-labelledby="tabpanel-405"
+        tabindex="0"
+      >
+        Sources !
+      </div>
+
+      <div
+        id="tabpanel-406-panel"
+        class="fr-tabs__panel"
+        role="tabpanel"
+        aria-labelledby="tabpanel-406"
+        tabindex="0"
+      >
+        Contenu !
+      </div>
+
+      <div
+        id="tabpanel-407-panel"
+        class="fr-tabs__panel"
+        role="tabpanel"
+        aria-labelledby="tabpanel-407"
+        tabindex="0"
+      >
+        Discussions !
+      </div>
+    </div>
   </div>
 </section>
