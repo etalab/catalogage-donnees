de3e29dff930c111d78c81a9bb514efb3f7591c6
florimondmanca <florimond.manca@fairness.coop>
2022-03-08 16:55:47 +0100

    Cleanup

--- client/src/tests/e2e/global-setup.js
+++ client/src/tests/e2e/global-setup.js
@@ -1,5 +1,6 @@
-import { firefox } from "@playwright/test";
+import { expect, firefox } from "@playwright/test";
 import { STATE_AUTHENTICATED, TEST_EMAIL, TEST_PASSWORD } from "./constants.js";
+
 /**
  * @param {import('@playwright/test').FullConfig} config
  */
@@ -21,7 +22,8 @@ async function saveAuthenticatedState(browser, config) {
   await page.fill("input[name='email']", TEST_EMAIL);
   await page.fill("input[name='password']", TEST_PASSWORD);
   await page.locator("button[type='submit']").click();
-  await page.waitForResponse("**/auth/login/");
+  const response = await page.waitForResponse("**/auth/login/");
+  expect(response.status()).toBe(200); // If this fails, ensure you ran `make initdata`.
   await page.waitForURL("/");
   await page.context().storageState({ path: STATE_AUTHENTICATED });
 }
