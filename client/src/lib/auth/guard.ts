182faf08498c427ad4aaba2f23054b13759f404c
florimondmanca <florimond.manca@fairness.coop>
2022-03-17 17:14:27 +0100

    Fix login redirect flash

--- client/src/lib/auth/guard.ts
+++ client/src/lib/auth/guard.ts
@@ -1,6 +1,5 @@
 import { get } from "svelte/store";
 import type { LoadOutput } from "@sveltejs/kit";
-import { browser } from "$app/env";
 import { isLoggedIn } from "../stores/auth";
 
 const isPublicPage = (url: URL): boolean => {
@@ -13,10 +12,6 @@ const isPublicPage = (url: URL): boolean => {
  */
 export const authGuard = (url: URL): LoadOutput => {
   if (isPublicPage(url)) {
-    return {};
-  }
-
-  if (!browser) {
     return {};
   }
 
