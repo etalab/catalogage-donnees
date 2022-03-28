7601263d5b1837b9bf1a8f9e19a88cea32cec791
florimondmanca <florimond.manca@fairness.coop>
2022-03-28 19:04:07 +0200

    Add basic error page

---
+++ client/src/routes/__error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts" context="module">
+  import type { ErrorLoad } from "@sveltejs/kit";
+
+  export const load: ErrorLoad = ({ error, status }) => {
+    return {
+      props: {
+        title: `${status}: ${error.message}`
+      }
+    };
+  };
+</script>
+
+<script lang="ts">
+  export let title: string;
+</script>
+
+<section class="fr-container fr-mt-9w">
+  <h1>{title}</h1>
+</section>
