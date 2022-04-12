6d8868bab5186e2a4331e40971c16502a1830f2b
Florimond Manca <florimond.manca@fairness.coop>
2022-04-12 12:17:43 +0200

    Treat password variables as secret (#173)

--- server/infrastructure/auth/passwords.py
+++ server/infrastructure/auth/passwords.py
@@ -1,4 +1,5 @@
 import argon2
+from pydantic import SecretStr
 
 from server.application.auth.passwords import PasswordEncoder
 
@@ -7,12 +8,12 @@ class Argon2PasswordEncoder(PasswordEncoder):
     def __init__(self) -> None:
         self._hasher = argon2.PasswordHasher()
 
-    def hash(self, value: str) -> str:
-        return self._hasher.hash(value)
+    def hash(self, password: SecretStr) -> str:
+        return self._hasher.hash(password.get_secret_value())
 
-    def verify(self, password: str, hash: str) -> bool:
+    def verify(self, password: SecretStr, hash: str) -> bool:
         try:
-            return self._hasher.verify(hash, password)
+            return self._hasher.verify(hash, password.get_secret_value())
         except argon2.exceptions.VerificationError:
             return False
         except argon2.exceptions.InvalidHash:
