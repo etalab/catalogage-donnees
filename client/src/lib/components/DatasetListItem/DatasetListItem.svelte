7f2682553c5cf6c73a7ca82b21ca58b25c596478
BertrandD <volubyl@pm.me>
2022-05-02 17:07:02 +0200

    refacto: make item clickable

--- client/src/lib/components/DatasetListItem/DatasetListItem.svelte
+++ client/src/lib/components/DatasetListItem/DatasetListItem.svelte
@@ -16,79 +16,79 @@
   };
 </script>
 
-<li class="fr-p-3w">
-  <p class="fr-logo fr-logo--sm fr-p-0" title="république française">
-    {@html "Ministère<br />de la culture"}
-  </p>
+<li class="fr-p-3w container">
+  <a
+    href={paths.datasetDetail({ id: dataset.id })}
+    title="Consulter cette fiche de données"
+  >
+    <p class="fr-logo fr-logo--sm fr-p-0" title="république française">
+      {@html "Ministère<br />de la culture"}
+    </p>
 
-  <div class="fr-container">
-    <p class="fr-m-0">
+    <div class="fr-container">
+      <p class="fr-m-0">
+        {#if dataset.headlines}
+          <strong data-testid="headlines-title">
+            {@html dataset.headlines.title}
+          </strong>
+        {:else}
+          <strong>
+            {dataset.title}
+          </strong>
+        {/if}
+      </p>
+
+      <p class="fr-m-0 fr-text--sm fr-text-mention--grey">
+        {dataset.service}
+      </p>
+
+      <div class="metadata-items fr-mt-1w">
+        <div>
+          <span class="fr-fi fr-fi-x-map-2-line" aria-hidden="true" />
+          <p class="fr-text--xs fr-my-0">
+            <span class="fr-text-mention--grey">Couverture géographique</span>
+            <br />
+            <span
+              >{GEOGRAPHICAL_COVERAGE_LABELS[
+                dataset.geographicalCoverage
+              ]}</span
+            >
+          </p>
+        </div>
+        <div>
+          <span class="fr-fi fr-fi-file-line" aria-hidden="true" />
+          <p class="fr-text--xs fr-my-0">
+            <span class="fr-text-mention--grey">Formats</span> <br />
+            <span>{formatFormats(dataset)}</span>
+          </p>
+        </div>
+      </div>
       {#if dataset.headlines}
-        <strong data-testid="headlines-title">
-          {@html dataset.headlines.title}
-        </strong>
-      {:else}
-        <strong>
-          {dataset.title}
-        </strong>
-      {/if}
-    </p>
-
-    <p class="fr-m-0 fr-text--sm fr-text-mention--grey">
-      {dataset.service}
-    </p>
-
-    <div class="metadata-items fr-mt-1w">
-      <div>
-        <span class="fr-fi fr-fi-x-map-2-line" aria-hidden="true" />
-        <p class="fr-text--xs fr-my-0">
-          <span class="fr-text-mention--grey">Couverture géographique</span>
-          <br />
-          <span
-            >{GEOGRAPHICAL_COVERAGE_LABELS[dataset.geographicalCoverage]}</span
+        <p class="fr-mb-0 fr-mt-1w fr-text--sm fr-text-mention--grey">
+          <em data-testid="headlines-description"
+            >... {@html dataset.headlines.description} ...</em
           >
         </p>
-      </div>
-      <div>
-        <span class="fr-fi fr-fi-file-line" aria-hidden="true" />
-        <p class="fr-text--xs fr-my-0">
-          <span class="fr-text-mention--grey">Formats</span> <br />
-          <span>{formatFormats(dataset)}</span>
-        </p>
-      </div>
+      {/if}
     </div>
-    {#if dataset.headlines}
-      <p class="fr-mb-0 fr-mt-1w fr-text--sm fr-text-mention--grey">
-        <em data-testid="headlines-description"
-          >... {@html dataset.headlines.description} ...</em
-        >
-      </p>
-    {/if}
-  </div>
 
-  <div class="actions">
-    <p class="fr-mb-2w fr-text--sm fr-text-mention--grey">
-      {capitalize(
-        formatDaysMonthsOrYearsToNow(dataset.catalogRecord.createdAt)
-      )}
-    </p>
-    <a
-      href={paths.datasetDetail({ id: dataset.id })}
-      class="fr-link fr-fi-arrow-right-line fr-link--icon-right"
-      title="Consulter cette fiche de données"
-    >
-      Voir
-    </a>
-  </div>
+    <div class="actions">
+      <p class="fr-mb-2w fr-text--sm">
+        {capitalize(
+          formatDaysMonthsOrYearsToNow(dataset.catalogRecord.createdAt)
+        )}
+      </p>
+
+      <span> Voir </span>
+    </div>
+  </a>
 </li>
 
 <style>
-  li {
-    display: grid;
-    grid-template-columns: auto 1fr auto;
-    column-gap: 2rem;
+  a {
+    display: flex;
+    width: 100%;
   }
-
   li:not(:last-child) {
     border-bottom: 1px solid var(--border-default-grey);
   }
@@ -115,5 +115,8 @@
 
   .actions {
     text-align: right;
+  }
+  [href] {
+    box-shadow: none;
   }
 </style>
