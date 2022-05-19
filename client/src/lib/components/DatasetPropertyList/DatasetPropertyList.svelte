648c8a46fb3b06722ada185957b02d469a93a88e
Florimond Manca <florimond.manca@fairness.coop>
2022-05-19 18:23:34 +0200

    Fix dataset list item alignment (#247)
    * Fix dataset list item alignment
    
    * Refactory: DatasetPropertyList

---
+++ client/src/lib/components/DatasetPropertyList/DatasetPropertyList.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  type Property = {
+    label: string;
+    icon: string;
+    value: string;
+  };
+
+  export let properties: Property[];
+</script>
+
+<div class="property-list fr-row">
+  {#each properties as { icon, label, value }}
+    <div class="property-list-item fr-col-{Math.floor(12 / properties.length)}">
+      <span class="fr-fi {icon}" aria-hidden="true" />
+
+      <div class="fr-text--xs fr-my-0 fr-ml-1w">
+        <div class="fr-text-mention--grey">
+          {label}
+        </div>
+        <div>
+          {value}
+        </div>
+      </div>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .property-list {
+    display: flex;
+    gap: var(--sp-2w);
+  }
+
+  .property-list-item {
+    display: flex;
+    align-items: center;
+  }
+
+  [class*="fr-fi"] {
+    color: var(--text-action-high-blue-france);
+    margin-top: 0.2rem; /* Adjust vertical alignment */
+  }
+</style>
