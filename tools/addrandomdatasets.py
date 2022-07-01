aa3f1849569f2098f0c6904526f7005f2587ff6c
florimondmanca <florimond.manca@fairness.coop>
2022-07-01 15:16:55 +0200

    Assign tags randomly

--- tools/addrandomdatasets.py
+++ tools/addrandomdatasets.py
@@ -1,6 +1,7 @@
 import argparse
 import asyncio
 import functools
+import random
 
 import aiometer
 import click
@@ -9,6 +10,7 @@ from tqdm import tqdm
 from server.config.di import bootstrap, resolve
 from server.seedwork.application.messages import MessageBus
 from tests.factories import CreateDatasetFactory
+from server.application.tags.queries import GetAllTags
 
 success = functools.partial(click.style, fg="bright_green")
 
@@ -16,10 +18,13 @@ success = functools.partial(click.style, fg="bright_green")
 async def main(n: int) -> None:
     bus = resolve(MessageBus)
 
+    tag_id_set = [tag.id for tag in await bus.execute(GetAllTags())]
+
     with tqdm(total=n, unit="dataset") as progress:
 
         async def task() -> None:
-            await bus.execute(CreateDatasetFactory.build())
+            tag_ids = random.choices(tag_id_set, k=random.randint(1, 3))
+            await bus.execute(CreateDatasetFactory.build(tag_ids=tag_ids))
             progress.update()
 
         tasks = [task for _ in range(n)]
