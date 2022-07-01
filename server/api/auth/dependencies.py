d5aa2b64ba4df23e12a58c80e54d83d18a97b7ce
florimondmanca <florimond.manca@fairness.coop>
2022-07-01 13:44:42 +0200

    Fix OpenAPI docs security scheme binding

--- server/api/auth/dependencies.py
+++ server/api/auth/dependencies.py
@@ -1,17 +1,52 @@
-from fastapi import HTTPException
+from fastapi import Depends, HTTPException
+from fastapi.security.base import SecurityBase
 
 from server.domain.auth.entities import UserRole
 
+from ..resources import auth_backend
 from ..types import APIRequest
 
 
 class IsAuthenticated:
-    def __call__(self, request: APIRequest) -> None:
+    """
+    Require requests to be authenticated.
+
+    Usage:
+        @router.get(
+            ...,
+            dependencies=[
+                Depends(IsAuthenticated()),
+            ],
+        )
+    """
+
+    def __call__(
+        self,
+        request: APIRequest,
+        # Make FastAPI generated API docs register the security scheme,
+        # and attach it to any route that has `Depends(IsAuthenticated())`.
+        _: SecurityBase = Depends(auth_backend.security),
+    ) -> None:
         if not request.user.is_authenticated:
             raise HTTPException(401, detail="Invalid credentials")
 
 
-class HasRole(IsAuthenticated):
+class HasRole:
+    """
+    An add-on to `IsAuthenticated()` which requires to authenticated user to
+    have specific roles.
+
+    Usage:
+
+        @router.get(
+            ...,
+            dependencies=[
+                Depends(IsAuthenticated()),
+                Depends(HasRole(...)),
+            ],
+        )
+    """
+
     def __init__(self, *roles: UserRole) -> None:
         self._roles = roles
 
