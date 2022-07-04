c4fec410a5cfb4a05e81c64a50723592ebb68736
BertrandD <volubyl@pm.me>
2022-07-04 09:57:49 +0200

    fix: click outside behavior in SearchableSelect

--- client/src/lib/components/SearchableSelect/SearchableSelect.svelte
+++ client/src/lib/components/SearchableSelect/SearchableSelect.svelte
@@ -1,62 +1,62 @@
 <script lang="ts">
   import type { SelectOption } from "src/definitions/form";
-  import { createEventDispatcher } from "svelte";
   import { clickOutside } from "src/lib/actions/clickOutside";
-
+  import { slugify } from "src/lib/util/format";
+  import { createEventDispatcher } from "svelte";
   export let buttonPlaceholder: string;
   export let inputPlaceholder: string;
+  export let label: string;
   export let options: Array<SelectOption>;
-
   let macthedOptions: Array<SelectOption> = options;
   let isOverlayOpen = false;
   let buttonText = buttonPlaceholder;
-
   let searchTerm: string;
-
   $: macthedOptions = options.filter((item) =>
     item.label.match(new RegExp(searchTerm, "i"))
   );
-
+  $: slug = slugify(label);
   const openOverlay = () => {
     isOverlayOpen = true;
   };
-
   const closeOverlay = () => {
     macthedOptions = options;
     isOverlayOpen = false;
   };
-
   const handleOverlayOpening = () => {
     if (!isOverlayOpen) {
       openOverlay();
       return;
     }
-
     if (isOverlayOpen) {
       closeOverlay();
       return;
     }
   };
-
   const dispatch = createEventDispatcher<{
     clickItem: SelectOption | null;
   }>();
-
   const handleClickListItem = (option: SelectOption) => {
     buttonText = option.label;
     dispatch("clickItem", option);
     closeOverlay();
   };
-
   const handleResetFilter = () => {
     macthedOptions = options;
     buttonText = buttonPlaceholder;
     dispatch("clickItem", null);
+    closeOverlay();
   };
 </script>
 
-<div class="container" use:clickOutside={{ callback: handleOverlayOpening }}>
-  <button class="fr-select" on:click={handleOverlayOpening}>{buttonText}</button
+<div use:clickOutside={{ callback: () => closeOverlay() }} class="container">
+  <label data-testId={`${slug}-label`} id={`${slug}-label`} for={slug}
+    >{label}</label
+  >
+  <button
+    aria-labelledby={`${slug}-label`}
+    class="fr-select"
+    data-testId={`${slug}-button`}
+    on:click={handleOverlayOpening}>{buttonText}</button
   >
 
   {#if isOverlayOpen}
@@ -67,14 +67,15 @@
         bind:value={searchTerm}
         type="search"
       />
+
       <ul>
+        {#if macthedOptions.length > 0}
+          <li on:click={handleResetFilter}>Réinitialiser le filtre</li>
+        {/if}
+
         {#each macthedOptions as option}
           <li on:click={() => handleClickListItem(option)}>{option.label}</li>
         {/each}
-
-        {#if macthedOptions.length > 0}
-          <li on:click={handleResetFilter}>Réinitiliser le filtre</li>
-        {/if}
 
         {#if macthedOptions.length === 0}
           <li class="no-result">Aucun résultat trouvé</li>
@@ -85,6 +86,9 @@
 </div>
 
 <style>
+  button {
+    text-align: left;
+  }
   .container {
     position: relative;
     box-sizing: border-box;
@@ -101,29 +105,24 @@
     position: absolute;
     border: 1px solid var(--background-contrast-grey);
   }
-
   input {
     margin: 0;
     margin-top: 5px;
   }
-
   ul {
     list-style-type: none;
     padding: 0;
     margin: 0;
   }
-
   li {
     cursor: pointer;
     padding: 8px 16px;
     margin: 0;
     border-top: 1px solid var(--background-contrast-grey);
   }
-
   li:hover {
     background-color: var(--background-contrast-grey);
   }
-
   .no-result {
     cursor: not-allowed;
   }
--- client/src/lib/util/format.ts
+++ client/src/lib/util/format.ts
@@ -51,3 +51,32 @@ export const formatDaysMonthsOrYearsToNow = (date: Date): string => {
 export const splitParagraphs = (text: string): string[] => {
   return text.split("\n");
 };
+
+export const slugify = (str: string): string => {
+  str = str.replace(/^\s+|\s+$/g, "");
+
+  // Make the string lowercase
+  str = str.toLowerCase();
+
+  // Remove accents, swap ñ for n, etc
+  const from =
+    "ÁÄÂÀÃÅČÇĆĎÉĚËÈÊẼĔȆÍÌÎÏŇÑÓÖÒÔÕØŘŔŠŤÚŮÜÙÛÝŸŽáäâàãåčçćďéěëèêẽĕȇíìîïňñóöòôõøðřŕšťúůüùûýÿžþÞĐđßÆa·/_,:;".split(
+      ""
+    );
+  const to =
+    "AAAAAACCCDEEEEEEEEIIIINNOOOOOORRSTUUUUUYYZaaaaaacccdeeeeeeeeiiiinnooooooorrstuuuuuyyzbBDdBAa------".split(
+      ""
+    );
+  from.forEach((_, index) => {
+    str = str.replace(new RegExp(from[index], "g"), to[index]);
+  });
+
+  str = str
+    .replace(/[^a-z0-9 -]/g, "")
+    // Collapse whitespace and replace by -
+    .replace(/\s+/g, "-")
+    // Collapse dashes
+    .replace(/-+/g, "-");
+
+  return str;
+};
