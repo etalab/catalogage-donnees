bc55c0c26d191fd3f4a6c92c5fa1ece460fd2319
Volubyl <volubyl@pm.me>
2022-07-04 11:50:59 +0200

    refacto: implement pagination for search page (#326)

---
+++ client/src/routes/fiches/_PaginationContainer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Pagination from "src/lib/components/Pagination/Pagination.svelte";
+  export let currentPage: number;
+  export let totalPages: number;
+  export let getPageLink: (page: number) => string;
+</script>
+
+<div class="pagination-container fr-mt-2w ">
+  <Pagination {currentPage} {totalPages} {getPageLink} />
+</div>
+
+<style>
+  .pagination-container {
+    display: flex;
+    justify-content: space-around;
+  }
+</style>
